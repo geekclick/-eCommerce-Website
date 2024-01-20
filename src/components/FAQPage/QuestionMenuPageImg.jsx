b970513e0deaf165ca7f199c3b5b60d67c1ac099
nedaashkan <117584245+nedaashkan@users.noreply.github.com>
2024-01-20 16:28:13 +0330

    add

--- src/components/FAQPage/QuestionMenuPageImg.jsx
+++ src/components/FAQPage/QuestionMenuPageImg.jsx
@@ -3,7 +3,7 @@ import { FaRegQuestionCircle } from "react-icons/fa";
 function QuestionMenuPageImg() {
   return (
     <div className="QuestionMenuPageImg  bg-black mt-16">
-      <div className="bg-[url('QuestionPage/1.png')] bg-cover h-20 lg:h-44 flex items-center justify-center  md:h-32 m-auto">
+      <div className="bg-[url('./QuestionPage/1.png')] bg-cover h-20 lg:h-44 flex items-center justify-center  md:h-32 m-auto">
         <h1 className="text-[0.9rem] md:text-[1.3rem]  flex  items-center gap-x-4 lg:text-[3rem] text-center text-white font-bold drop-shadow-lg">
           <FaRegQuestionCircle /> FAQ
         </h1>
