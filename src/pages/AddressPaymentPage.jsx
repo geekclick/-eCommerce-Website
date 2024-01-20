106ff208c3de316f43d8fd0612acffe592ba5d83
geekclick <anujghom27@gmail.com>
2024-01-20 18:38:39 +0530

    revert this commit

--- src/pages/AddressPaymentPage.jsx
+++ src/pages/AddressPaymentPage.jsx
@@ -5,7 +5,7 @@ export default function AddressPaymentPage() {
   return (
     <div className="AddressPaymentPage bg-black">
       <h1 className="text-[2.5rem] md:text-[3rem] lg:text-[4rem] text-center text-[#00d5ff] mb-5 lg:mb-1 md:mb-5">
-        trendyol
+        trendyol.
       </h1>
       <div className="flex flex-col lg:flex-row md:flex-row justify-center items-center lg:items-start md:items-start lg:space-x-2 md:space-x-2 space-x-0 lg:mt-20 md:mt-10">
         <ShoppingCartCost2 />
